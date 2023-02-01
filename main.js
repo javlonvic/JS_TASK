2f29f9dbfdb6f8739908b067168c7f8310b425c1
abdulaziz <olimboyevabdulaziz@gmail.com>
2023-02-01 09:31:15 +0500

    JS_Task

---
+++ main.js
@@ -0,0 +1,23 @@
+let username = prompt('What is your name ?')
+if (username <= 6) {
+    let people = prompt('How many you are ?')
+    if (people >= 10) {
+        let money = ('We have rule, you need to pay 1000 per each')
+        if (money = 1000) {
+           let order = ('Do you want to order something? Menu: 1.Coca Cola, 2.Shawarma, 3.Teablack, 4.Skewer')} 
+
+
+          else {
+            console.log("You don't have enough funds");
+        }
+
+    } else  {
+        console.log("Error try again");
+    }
+
+} else {
+ console.log("Error try again");
+}
+
+
+
